8675a321d236a1cc3eeab67dc82b5e82046c3412
safetest-dev <safetestmail@proton.me>
2026-01-14 17:20:59 +0700

    Create text.go

---
+++ pkg/output/text.go
@@ -0,0 +1,23 @@
+package output
+
+import (
+	"fmt"
+
+	"github.com/username/extractor-recon/internal/scanner"
+)
+
+func PrintText(results []scanner.Result) {
+	for _, r := range results {
+		fmt.Printf("[+] %s\n", r.URL)
+		fmt.Printf("[+] Status: %d %s\n", r.Status, r.StatusText)
+
+		for _, l := range r.Links {
+			if l.Error != "" {
+				fmt.Printf("  [-] %s : ERROR (%s)\n", l.URL, l.Error)
+			} else {
+				fmt.Printf("  [-] %s : %d %s\n",
+					l.URL, l.Status, l.StatusText)
+			}
+		}
+	}
+}
