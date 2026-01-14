038d87ca9f70b0ded053a400e150c57ae792ba6f
safetest-dev <safetestmail@proton.me>
2026-01-14 17:18:35 +0700

    Create probe.go

---
+++ internal/probe/probe.go
@@ -0,0 +1,32 @@
+package probe
+
+import (
+	"net/http"
+
+	"github.com/username/extractor-recon/internal/scanner"
+)
+
+type Prober struct {
+	Client *http.Client
+}
+
+func New(client *http.Client) *Prober {
+	return &Prober{Client: client}
+}
+
+func (p *Prober) Check(url string) scanner.LinkResult {
+	resp, err := p.Client.Get(url)
+	if err != nil {
+		return scanner.LinkResult{
+			URL:   url,
+			Error: err.Error(),
+		}
+	}
+	defer resp.Body.Close()
+
+	return scanner.LinkResult{
+		URL:        url,
+		Status:     resp.StatusCode,
+		StatusText: http.StatusText(resp.StatusCode),
+	}
+}
